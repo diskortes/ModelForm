10471234636d5bd57c39270cc57cdf8f323e3ee2
I D <skazochnik@email.ua>
2011-10-17 10:39:05 +0300

    Changed AuthorView

--- books/views.py
+++ books/views.py
@@ -1,10 +1,12 @@
-from ModelForm.books.models import AuthorForm
+from ModelForm.books.models import AuthorForm, Author
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 
 def AuthorView(request):
-    form = AuthorForm(request.POST)
-    if request.method == 'POST':
+    i = Author.objects.get(id=1)
+    form = AuthorForm(instance=i)
+    if request.method == 'POST':   
+        form = AuthorForm(request.POST, instance=i)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/bye/')
